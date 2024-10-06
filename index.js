b2d11188831e3a0c79146665278580b5cf95e51e
TheLazyCodernothacker <lockemaximus@yahoo.com>
2024-10-06 15:24:35 -0700

    added express to redirect to blank page

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const Dare = require("./models/dare");
 const Truth = require("./models/truth");
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 const {
   SlashCommandBuilder,
